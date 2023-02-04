967fe4fb2793a3c175d920d8715f7aef3bc9f24f
Vitinho369 <vitinho21.2005@gmail.com>
2023-02-04 15:31:36 -0300

    correcoes

--- game/pages/button_navigate.js
+++ game/pages/button_navigate.js
@@ -1,20 +1,20 @@
 function navigateGame(){
     var url = window.location.pathname.split('index.html');
     var namePage = url.pop();
-    url += '/pages/gamePage.html';
+    url += '/game/pages/gamePage.html';
     window.location.href =   url;
 }
 
 function navigateTutorial(){
     var url = window.location.pathname.split('index.html');
     var namePage = url.pop();
-    url += '/pages/tutorial.html';
+    url += '/game/pages/tutorial.html';
     window.location.href =   url;
 }
 
 function navigateAboutUs(){
     var url = window.location.pathname.split('index.html');
     var namePage = url.pop();
-    url += '/pages/aboutUs.html';
+    url += '/game/pages/aboutUs.html';
     window.location.href =   url;
 }
