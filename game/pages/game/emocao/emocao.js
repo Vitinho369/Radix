a13e4533f2b0107f7c39dd0b72add8b6056e95b4
luanmagioli <luanmagioli@gmail.com>
2023-02-05 14:19:36 +0000

    meu último commit graças a deus sqn

--- game/pages/game/emocao/emocao.js
+++ game/pages/game/emocao/emocao.js
@@ -1,12 +1,17 @@
 class Emocao {
-
-
-    //cenário da emoção, vai ficar no fundo do jogo com o boneco do jogador
     constructor() {
         this.bodys = [
-            loadImage('../resources/characters/body1.png'),
-            
+            loadImage('../../../resources/emocao/sad_clothes.png'),
+            loadImage('../../../resources/emocao/clothes_neutro.png'),
+            loadImage('../../../resources/emocao/clothe_happy.png'),
         ];
+
+        this.heads = [
+            loadImage('../../../resources/emocao/sad_head.png'),
+            loadImage('../../../resources/emocao/neutra_head.png'),
+            loadImage('../../../resources/emocao/happy2_head.png'),
+        ];
+
         this.status = 0;
         this.cores = [[[41, 72, 92], [254, 223, 171]],
         [[216, 104, 95], [171, 146, 134]],
@@ -15,8 +20,15 @@ class Emocao {
     }
 
     show() {
-        //carrega imagem do corpo do personagem
-        //background(255);
+        //coloca o corpo do personagem (dependendo do status) no centro da tela
+        //cria a variaável offset para fazer a animação de bounce
+        let offset = 0;
+        offset = map(sin(frameCount / 10), -1, 1, -5, 5);
+        image(this.bodys[this.status], width / 2 - (this.bodys[this.status].width / 2), height / 2 - (this.bodys[this.status].height / 2.5) + offset);
+        
+        let offset2 = map(sin((frameCount-6) / 10), -1, 1, -8, 8);
+        //coloca a cabeça do personagem (dependendo do status) em cima do corpo do personagem com um offset levemente atrasado
+        image(this.heads[this.status], width / 2 - (this.heads[this.status].width / 2), height / 2 - (this.heads[this.status].height / 2.5) + offset2 - 550);
     }
 
     
--- game/pages/game/game.js
+++ game/pages/game/game.js
@@ -1,36 +1,74 @@
-let wheel = null;
-let cenario = [];
+socket = io();
+
+let cenario;
+let cards = [];
+let painel;
+
+let vezdejogar = false;
 
 function preload() {
+  painel = loadImage('../../../resources/painel.png');
   cenario = new Emocao();
-  emocao = 0;
+
+  //se comunica ao servidor para pegar as cartas (se receber empty, não é a vez dele)
+  loadCards();
+}
+
+
+function loadCards(){
+  //carrega as cartas
+  socket.on('cards', (cards) => {
+    if(cards != "empty"){
+      for(let i = 0; i < cards.length; i++){
+        cards[i] = new Carta(cards[i]);
+      }
+      vezdejogar = true;
+    }else{      
+      vezdejogar = false;
+    }
+  });
+  socket.on('cenario', function(status){
+    cenario.status = status;
+  });
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  wheel = new Wheel();
 }
 
 function draw() {
-  cenario.show();
   textSize(width / 14);
   strokeWeight(0.5);
   
   paintbackground();
+  
 
-  textAlign(CENTER, TOP);
-  text('TOP', 0, width / 20, width);
+  cenario.show();
 
-  wheel.show();
+  //mostrar o painel das cartas imagem panel.png
+  image(painel, width/2 - ((painel.width)/2), height- 700, width+10, width);
+  
+  if(vezdejogar){
+    for(let i = 0; i < cards.length; i++){
+      cards[i].show();
+    }
+  }else{
+    textFont('Poppins');
+    textStyle(BOLD);
+    textAlign(CENTER);
+    fill(0,0,0,100);
+
+    text("Aguarde sua vez...", width/2, height-200);
+  }
+  
 }
 
 
 function paintbackground(){
   noStroke();
-  console.log(cenario.cores)
-  fill(random(cenario.cores[emocao][0][0]-20, cenario.cores[emocao][0][0]+20),
-       random(cenario.cores[emocao][0][1]-20, cenario.cores[emocao][0][1]+20),
-       random(cenario.cores[emocao][0][2]-20, cenario.cores[emocao][0][2]+20),6)
+  fill(random(cenario.cores[cenario.status][0][0]-20, cenario.cores[cenario.status][0][0]+20),
+       random(cenario.cores[cenario.status][0][1]-20, cenario.cores[cenario.status][0][1]+20),
+       random(cenario.cores[cenario.status][0][2]-20, cenario.cores[cenario.status][0][2]+20),6)
   ellipse(random(0,width),random(0,height),random(0,width),random(0,height))
   ellipse(random(0,width),random(0,height/3),random(0,width),random(0,height/3))
   ellipse(random(0,width),random(0,height/2),random(0,width),random(0,height/2))
@@ -38,24 +76,35 @@ function paintbackground(){
   ellipse(random(0,width),random(0,height/3),random(0,width),random(0,height/3))
   fill(255,255,255,1.8*cos(frameCount))
 
-  fill(random(cenario.cores[emocao][1][0]-20, cenario.cores[emocao][1][0]+20),
-       random(cenario.cores[emocao][1][1]-20, cenario.cores[emocao][1][1]+20),
-       random(cenario.cores[emocao][1][2]-20, cenario.cores[emocao][1][2]+20),4)
+  fill(random(cenario.cores[cenario.status][1][0]-20, cenario.cores[cenario.status][1][0]+20),
+       random(cenario.cores[cenario.status][1][1]-20, cenario.cores[cenario.status][1][1]+20),
+       random(cenario.cores[cenario.status][1][2]-20, cenario.cores[cenario.status][1][2]+20),4)
   ellipse(random(0,width),random(random((height/3)*2), height),random(random(0, width/4),random(width/4*3, width)),random(random((height/3), (height/3)*2), random(random(0, width/4),random(width/4*3, width))))  
 }
 
-function touchStarted() {
-  wheel.touchStarted();
+
+function mouseClicked() {
+  background(255);
+  cenario.status = (cenario.status + 1) % 3;
 }
 
-function touchMoved() {
-  wheel.touchMoved();
-}
+//classe "card", uma interface de carta com a função show (que mostra uma imagem no inferior da tela (esquerdo, centro ou direito dependendo da posição do card))
+class Card {
+  constructor(img, pos, cardjson) {
+    this.img = img;
+    this.card = cardjson;
+    this.pos = pos;
+  }
 
-function touchEnded() {
-  wheel.touchEnded();
-}
+  show() {
+    image(this.img, width / 3 * this.pos - this.img.width / 2, height - this.img.height);
+  }
 
-function mousePressed() {
-  emocao = (emocao + 1) % 3;
+  //função que encontra o card que foi clicado
+  onMouseClick() {
+    if (mouseX > width / 3 * this.pos - this.img.width / 2 && mouseX < width / 3 * this.pos + this.img.width / 2 && mouseY > height - this.img.height && mouseY < height) {
+      //se comunica com o servidor para enviar o card clicado
+      socket.emit('jogarCarta', this.card);
+    }
+  }
 }
